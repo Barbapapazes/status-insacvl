3fff7db23b37d985dc90b9b01669cbe80a71e6db
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-12-01 23:13:37 +0100

    feat(logger): add logger to cron

--- start/cron.ts
+++ start/cron.ts
@@ -3,6 +3,7 @@ import Env from '@ioc:Adonis/Core/Env'
 import cron from 'node-cron'
 import { request } from 'undici'
 import Service from 'App/Models/Service'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 const time = Env.get('CRON_TIME') || '*/5 * * * *'
 
@@ -12,9 +13,12 @@ async function start() {
   try {
     const services = await Service.all()
     services.forEach((service) => {
+      Logger.info(`Starting cron from service "${service.name}"`)
       cron.schedule(time, getServiceStatus(service.id, service.url))
     })
-  } catch (error) {}
+  } catch (error) {
+    Logger.error(error)
+  }
 }
 
 function getServiceStatus(serviceId: number, url: string) {
